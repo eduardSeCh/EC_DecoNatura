906b76a342623ec4bf70844d2b31eba89423f5fb
eduard <eduardogsech@gmail.com>
2023-02-12 17:11:28 -0600

    prueba de api unplash

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,47 @@
 //Colocar productos -> pagina principal
+const accesKey = 'AZLVkjSoxJlIBqWUcAs9gjPFGLhyA7NIknzqPJ0g2zQ'
+const endPoint = 'https://api.unsplash.com/photos/'
+
+async function getImages(query) {
+  let response = await fetch();
+  let jsonResponse = await response.json(`https://api.unsplash.com/search/photos/`);
+  let imageList = await jsonResponse.results;
+  console.log(imageList)
+  createImages(imageList);  
+}
+
+function createImages(imagesList) {
+  for (let i = 0; i < imagesList.length; i++) {
+    const img = document.createElement('img');
+    img.src = imagesList[i].urls.thumb;
+    document.body.appendChild(img);
+    
+  }
+}
+
+getImages('U3YWF2n_q-M');
+
+
 const addProducts = () => {
   fetch("../../productos.json")
     .then((response) => response.json())
-    .then((data) => { console.log(data[0].Producto)
+    .then((data) => { console.log(data[0].Categoria)
       for (let i = 0; i < 4; i++) {
-        console.log("Funciona? " + data[i].Producto);
+        //Productos destacados
         document.getElementById("productos").innerHTML += `
-                <div class="col">
-                    <img src="${data[i].Imagen}" alt="">
-                    <hr>
-                    <p class="precio">${data[i].Producto} Producto</p>
-                </div>`;
+            <div class="col">
+                <img src="${data[i].Imagen}" alt="">
+                <hr>
+                <p class="precio">${data[i].Producto}</p>
+            </div>`;
+
+        //Categorias
+        document.getElementById("categoria").innerHTML += `
+        <div class="col">
+            <img src="${data[i].Imagen}" alt="">
+            <hr>
+            <p class="precio">${data[i].Categoria}</p>
+        </div>`;
         }
     });
 };
